819253266c8220a41b07d022d3cec37c0bc67620
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-09 16:01:51 +0600

    second commit

--- Recursion | Sum of digits & power of a number/Recursion.java
+++ Recursion | Sum of digits & power of a number/Recursion.java
@@ -1,5 +1,12 @@
 // package Recursion | Sum of digits & power of a number;
 
 public class Recursion {
-    
+    public static int sod(int n){
+        if(n >= 0 && n <= 9) return n;
+        return sod(n/10) + n%10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sod(12345));
+    }
 }
