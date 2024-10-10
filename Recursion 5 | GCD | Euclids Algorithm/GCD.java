6a5d3eecf6643a09269bb6bec7a8600cddb1afc5
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-10 16:57:47 +0600

    second commit

--- Recursion 5 | GCD | Euclids Algorithm/GCD.java
+++ Recursion 5 | GCD | Euclids Algorithm/GCD.java
@@ -1,23 +1,23 @@
 // package Recursion | GCD | Euclids Algorithm;
 
-public class GCD {
-    public static int gcd(int x, int y) {
-        if(y==0) {
-            return x;
-        }
+// public class GCD {
+//     public static int gcd(int x, int y) {
+//         if(y==0) {
+//             return x;
+//         }
 
-           // if(x%y == 0) {  // optional base case
-           //     return y;
-           // }
+//            // if(x%y == 0) {  // optional base case
+//            //     return y;
+//            // }
 
-        return gcd(y, x%y);
-    }
+//         return gcd(y, x%y);
+//     }
 
 
-    public static void main(String[] args) {
-        System.out.println("GCD is : " + gcd(24, 15));
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("GCD is : " + gcd(24, 15));
+//     }
+// }
 
 // output:
 // GCD is : 3
