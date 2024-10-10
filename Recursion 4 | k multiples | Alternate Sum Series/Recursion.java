807bf27736ba37074c2cf1ee96e80c7f0e905009
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-10 16:56:02 +0600

    second commit

--- Recursion 4 | k multiples | Alternate Sum Series/Recursion.java
+++ Recursion 4 | k multiples | Alternate Sum Series/Recursion.java
@@ -1,5 +1,25 @@
 // package Recursion | k multiples | Alternate Sum Series;
 
+import java.util.Scanner;
+
 public class Recursion {
-    
+    static void printMultiples(int n, int k){ // 5, 4
+        //base case
+        if(k == 0){
+            return;
+        }
+        //recursive work
+        printMultiples(n, k-1); // 5, 10, 15
+        //self work
+        System.out.println(n * k); // 20
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        printMultiples(n, k);
+
+        sc.close();
+    }
 }
