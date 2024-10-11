e17a0da01720caa21d822309990b86641a5412b2
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-11 15:12:06 +0600

    second commit

--- Recursion 6 | on Arrays | Max Value | Sum of arrays/Recursion.java
+++ Recursion 6 | on Arrays | Max Value | Sum of arrays/Recursion.java
@@ -1,5 +1,21 @@
 // package Recursion on Arrays | Max Value | Sum of arrays;
 
-public class Recursion {
-    
-}
+// public class Recursion {
+//     public static void array(int arr[], int idx) {
+//         if (idx == arr.length) {
+//             return;
+//         }
+
+//         System.out.print(arr[idx] + " ");
+//         array(arr, idx + 1);
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = { 5, 6, 7, 8 };
+//         array(arr, 0);
+//     }
+
+// }
+
+// output:
+// 5 6 7 8
