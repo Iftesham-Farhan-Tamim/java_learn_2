848c5d4eaa75cf54643d074b548ffa785425c1b9
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-11 22:09:59 +0600

    second commit

--- Recursion 6 | on Arrays | Max Value | Sum of arrays/Recursion.java
+++ Recursion 6 | on Arrays | Max Value | Sum of arrays/Recursion.java
@@ -1,7 +1,7 @@
 // package Recursion on Arrays | Max Value | Sum of arrays;
 
 // public class Recursion {
-//     public static void array(int arr[], int idx) {
+//     public static void print(int arr[], int idx) {
 //         if (idx == arr.length) {
 //             return;
 //         }
@@ -12,10 +12,51 @@
 
 //     public static void main(String[] args) {
 //         int arr[] = { 5, 6, 7, 8 };
-//         array(arr, 0);
+//         print(arr, 0);
 //     }
-
 // }
 
 // output:
 // 5 6 7 8
+// ............................................................................................
+// public class Recursion {
+//     static int max(int[] arr, int idx){
+//         //base case
+//         if(idx == arr.length-1){
+//             return arr[idx];
+//         }
+
+//         // small problem -> idx+1, end of the array -> max -> recursive
+//         int smallAns = max(arr, idx+1);
+
+//         // self work and final ans
+//         return Math.max(arr[idx], smallAns);
+//     }
+
+//     public static void main(String[] args) {
+//         int [] arr = {5, 6, 7, 8};
+//         System.out.println(max(arr, 0));
+//     }
+// }
+
+// output:
+// 8
+// ............................................................................................
+// public class Recursion {
+//     public static int sum(int arr[], int idx) {
+//         if(idx == arr.length-1) {
+//             return arr[idx];
+//         }
+
+//         int ans = sum(arr, idx+1);
+//         return ans + arr[idx];
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = {2,3,5,20,1};
+//         System.out.print(sum(arr, 0));
+//     }
+// }
+
+// output:
+// 31
