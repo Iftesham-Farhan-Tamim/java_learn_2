dd3606677291e293e041f1372ec21712d99e4a78
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-12 19:47:49 +0600

    second commit

--- Recursion 8 | on Strings  | Remove occurences/Recursion.java
+++ Recursion 8 | on Strings  | Remove occurences/Recursion.java
@@ -1,5 +1,25 @@
 // package Recursion | GCD | Euclids Algorithm;
 
-// public class Recursion {
+public class Recursion {
+    public static String remove(String s, int idx) {
+        if(idx == s.length()) {
+            return "";
+        }
+        
+        String smallAns = remove(s, idx+1);
+        char currChar = s.charAt(idx);
+        
+        if(currChar != 'a') {
+            return currChar + smallAns;
+        } else {
+            return smallAns;
+        }
+    }
     
-// }
+    public static void main(String[] args) {
+        System.out.print(remove("abcdea", 0));
+    }
+}
+
+// output:
+// bcde
