2aa0c577b928ef9edf229d3b9b0ed4e7c7b6ee22
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-12 22:54:42 +0600

    second commit

--- Recursion 8 | on Strings  | Remove occurences/Recursion.java
+++ Recursion 8 | on Strings  | Remove occurences/Recursion.java
@@ -23,3 +23,32 @@
 
 // output:
 // bcde
+// ............................................................................................
+// public class Recursion {
+//     public static String remove(String str) {
+//         if(str.length() == 0) {
+//             return "";
+//         }
+        
+//         String random = remove(str.substring(1));
+//         char currChar = str.charAt(0);
+        
+//         if(currChar != 'a') {
+//             return currChar + random;
+//         } else {
+//             return random;
+//         }
+//     }
+    
+//     public static void main(String[] args) {
+//         System.out.print(remove("abcda"));
+//     }
+// }
+
+// output:
+// bcd
+// ............................................................................................
+
+// ............................................................................................
+
+// ............................................................................................
