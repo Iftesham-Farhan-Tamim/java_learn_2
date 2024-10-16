f0fcdc9a797f1af03a77b115f44b3b5bdec75b11
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-16 23:23:01 +0600

    second commit

--- Recursion 9 | Find Subsequences | Subsets/Main.java
+++ Recursion 9 | Find Subsequences | Subsets/Main.java
@@ -1,26 +1,43 @@
-import java.util.ArrayList;
-public class Main {
+// import java.util.ArrayList;
+// public class Main {
 
-    static ArrayList<String> getSSQ(String s){
-        ArrayList<String> ans = new ArrayList<>();
-        // base case
-        if(s.length() == 0){
-            ans.add("");
-            return ans;
-        }
-        char curr = s.charAt(0); 
-        ArrayList<String> smallAns = getSSQ(s.substring(1)); 
+//     static ArrayList<String> getSSQ(String s){
+//         ArrayList<String> ans = new ArrayList<>();
+//         // base case
+//         if(s.length() == 0){
+//             ans.add("");
+//             return ans;
+//         }
+//         char curr = s.charAt(0); 
+//         ArrayList<String> smallAns = getSSQ(s.substring(1)); 
         
-        for(String ss: smallAns){
-            ans.add(ss); 
-            ans.add(curr + ss);  
+//         for(String ss: smallAns){
+//             ans.add(ss); 
+//             ans.add(curr + ss);  
+//         }
+//         return ans;
+//     }
+//     public static void main(String[] args) {
+//         ArrayList<String> ans = getSSQ("abc");
+//         for(String ss : ans){
+//             System.out.println(ss);
+//         }
+//     }
+// }
+// ............................................................................................
+public class Main {
+    static void printSSQ(String s, String currAns){
+        if(s.length() == 0){
+            System.out.println(currAns);
+            return;
         }
-        return ans;
+        char curr = s.charAt(0);
+        String remString = s.substring(1);
+        printSSQ(remString, currAns + curr); // add curr
+        printSSQ(remString, currAns);  // do not add curr
     }
+
     public static void main(String[] args) {
-        ArrayList<String> ans = getSSQ("abc");
-        for(String ss : ans){
-            System.out.println(ss);
-        }
+        printSSQ("abc", "");
     }
 }
