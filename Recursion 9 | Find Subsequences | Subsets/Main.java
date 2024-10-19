5072f3e8361ea56bd433423e05a6f71200053d58
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-19 18:06:02 +0600

    second commit

--- Recursion 9 | Find Subsequences | Subsets/Main.java
+++ Recursion 9 | Find Subsequences | Subsets/Main.java
@@ -98,22 +98,22 @@
 // c
 //  (empty string)
 // ............................................................................................
-public class Main {
-    static void subsetSum(int[] a, int n, int idx, int sum) {
-        if(idx == n) {
-            System.out.println(sum);
-            return;
-        }
+// public class Main {
+//     static void subsetSum(int[] a, int n, int idx, int sum) {
+//         if(idx == n) {
+//             System.out.println(sum);
+//             return;
+//         }
         
-        subsetSum(a, n, idx+1, sum + a[idx]);
-        subsetSum(a, n, idx+1, sum);
-    }
+//         subsetSum(a, n, idx+1, sum + a[idx]);
+//         subsetSum(a, n, idx+1, sum);
+//     }
 
-    public static void main(String[] args) {
-        int[] a = {2, 4, 5};  // The length of the array {2, 4, 5} is 3, as it contains 3 elements.
-        subsetSum(a, a.length, 0, 0);
-    }
-}
+//     public static void main(String[] args) {
+//         int[] a = {2, 4, 5};  // The length of the array {2, 4, 5} is 3, as it contains 3 elements.
+//         subsetSum(a, a.length, 0, 0);
+//     }
+// }
 
 // output:
 // 11
