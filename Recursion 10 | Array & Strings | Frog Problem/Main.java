dfa9473cec419bd2d0b7ef53b4f26c31d2710542
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-20 17:23:24 +0600

    second commit

---
+++ Recursion 10 | Array & Strings | Frog Problem/Main.java
@@ -0,0 +1,27 @@
+// package Recursion | Recursion on Array & Strings | Frog Problem;
+
+public class Main {
+    public static int jump(int arr[], int idx) {
+        if(idx == arr.length-1) {
+            return 0;
+        }
+        
+        int op1 = jump(arr, idx+1) + Math.abs(arr[idx+1] - arr[idx]);
+        
+        if(idx == arr.length-2) {
+            return op1;
+        }
+        
+        int op2 = jump(arr, idx+2) + Math.abs(arr[idx+2] - arr[idx]);
+        
+        return Math.min(op1, op2);
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {10, 30, 40, 20};
+        System.out.print(jump(arr, 0));
+    }
+}
+
+// output:
+// 30
--- Recursion 10 | Array & Strings | Frog Problem/Recursion.java
+++
@@ -1,5 +0,0 @@
-// package Recursion | Recursion on Array & Strings | Frog Problem;
-
-public class Recursion {
-    
-}
