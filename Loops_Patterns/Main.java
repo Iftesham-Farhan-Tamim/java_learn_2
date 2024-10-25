0ca8b9a1fbdbe078e1a612d69f9c8477487e6806
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-26 02:14:43 +0600

    second commit

--- Loops_Patterns/Main.java
+++ Loops_Patterns/Main.java
@@ -1,5 +1,40 @@
 // package Loops_Patterns;
 
 // public class Main {
-    
+//     public static void main(String[] args) {
+        
+//         int n = 4;
+        
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n - i; j++) {
+//                 System.out.print(" " + " ");
+//             }
+            
+//             System.out.print(i + " ");
+            
+//             if (i < n) {
+                
+//                 for (int j = 1; j <= 2 * i - 3; j++) {
+//                     System.out.print(" " + " ");
+//                 }
+                
+//                 if (i > 1) {
+//                     System.out.print(i + " ");
+//                 }
+//             } else {
+                
+//                 for (int j = 1; j < 2 * i - 1; j++) {
+//                     System.out.print(i + " ");
+//                 }
+//             }
+            
+//             System.out.println();
+//         }
+//     }
 // }
+
+// output:
+//       1 
+//     2   2 
+//   3       3 
+// 4 4 4 4 4 4 4 
