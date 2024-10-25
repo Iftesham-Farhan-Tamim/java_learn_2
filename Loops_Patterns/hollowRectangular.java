ef824d4b3831ad03b90fa71e3260d18f2c6556d8
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-25 21:04:59 +0600

    second commit

---
+++ Loops_Patterns/hollowRectangular.java
@@ -0,0 +1,20 @@
+// package Loops_Patterns;
+
+public class hollowRectangular {
+    public static void main(String[] args) {
+        int r = 6;
+        int c = 4;
+         
+        for(int i=0; i<c; i++) {
+            for(int j=0; j<r; j++) {
+                if(i==0 || i==c-1 || j==0 || j==r-1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
