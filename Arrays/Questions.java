c5ffbdb05a30828a958eb3e6eb7364321f04ec6c
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-27 03:19:23 +0600

    second commit

--- Arrays/Questions.java
+++ Arrays/Questions.java
@@ -1,5 +1,63 @@
-package Arrays;
+// package Arrays;
 
-public class Questions {
-    
-}
+// //Calculate sum
+// public class Questions {
+//     public static void main(String[] args) {
+        
+//         int arr[] =  {1,2,3,4};
+//         int sum = 0;
+        
+//         for(int i=0; i<arr.length; i++) {
+//             sum+=arr[i];
+//         }
+//         System.out.print(sum);
+        
+//     }
+// }
+
+// output:
+// 10
+// ............................................................................................
+// //Find the maximum value out off all elements in the array
+// public class Questions {
+//     public static void main(String[] args) {
+        
+//         int arr[] =  {1,2,9,4};
+//         int ans = 0;
+        
+//         for(int i=0; i<arr.length; i++) {
+//             if(arr[i]>ans) {
+//                 ans=arr[i];
+//             }
+//         }
+//         System.out.print(ans);
+        
+//     }
+// }
+
+// output:
+// 9
+// ............................................................................................
+// //Search the given element x in the array. If present then 
+// //return the index else return -1 (Linear search)
+
+// public class Questions {
+//     public static void main(String[] args) {
+        
+//         int arr[] =  {1,2,9,4};
+//         int ans = -1;
+//         int x = 5;
+        
+//         for(int i=0; i<arr.length; i++) {
+//             if(x==arr[i]) {
+//                 ans = i;
+//                 break;
+//             } 
+//         }
+//         System.out.print(ans);
+        
+//     }
+// }
+
+// output:
+// -1
